5f435c616fba0aecdd547e575fd08aab23f229b5
djh <jd4136@columbia.edu>
2026-04-25 09:53:22 +0000

    [fix](kernel): make cpptrace optional and tweak AMX MoE call

--- kt-kernel/ext_bindings.cpp
+++ kt-kernel/ext_bindings.cpp
@@ -12,10 +12,16 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#include <cpptrace/cpptrace.hpp>
 #include <csignal>
 #include <cstddef>
 #include <cstring>
+
+#if __has_include(<cpptrace/cpptrace.hpp>)
+#include <cpptrace/cpptrace.hpp>
+#define KTRANSFORMERS_HAS_CPPTRACE 1
+#else
+#define KTRANSFORMERS_HAS_CPPTRACE 0
+#endif
 
 #include "cpu_backend/cpuinfer.h"
 #include "cpu_backend/worker_pool.h"
@@ -54,8 +60,8 @@ static const bool _is_plain_ = false;
 #if defined(__x86_64__)
 #include "operators/avx2/bf16-moe.hpp"
 #include "operators/avx2/fp8-moe.hpp"
-#include "operators/avx2/gptq_int4_avxvnni-moe.hpp"
 #include "operators/avx2/gptq_int4-moe.hpp"
+#include "operators/avx2/gptq_int4_avxvnni-moe.hpp"
 #endif
 
 #include <pybind11/stl.h>  // std::vector/std::pair/std::string conversions
@@ -73,7 +79,6 @@ static const bool _is_plain_ = false;
 
 namespace py = pybind11;
 using namespace pybind11::literals;
-
 
 py::object to_float_ptr(uintptr_t input_ptr, int size, ggml_type type) {
   if (type < 0 || type >= GGML_TYPE_COUNT) {
@@ -473,7 +478,6 @@ void bind_moe_module(py::module_& moe_module, const char* name) {
 }
 
 PYBIND11_MODULE(kt_kernel_ext, m) {
-
   py::class_<WorkerPool>(m, "WorkerPool").def(py::init<int>());
   py::class_<WorkerPoolConfig>(m, "WorkerPoolConfig")
       .def(py::init<>())
@@ -813,7 +817,7 @@ PYBIND11_MODULE(kt_kernel_ext, m) {
   bind_moe_module<AVX2_FP8_MOE_TP<avx2::GemmKernelAVX2FP8>>(moe_module, "AVX2FP8_MOE");
   bind_moe_module<AVX2_GPTQ_INT4_MOE_TP<avx2::GemmKernelAVX2GPTQInt4>>(moe_module, "AVX2GPTQInt4_MOE");
   bind_moe_module<AVXVNNI256_GPTQ_INT4_MOE_TP<avxvnni::GemmKernelAVXVNNI256GPTQInt4>>(moe_module,
-                                                                                        "AVXVNNI256GPTQInt4_MOE");
+                                                                                      "AVXVNNI256GPTQInt4_MOE");
 #endif
 
 #if defined(USE_MOE_KERNEL)
@@ -977,17 +981,21 @@ PYBIND11_MODULE(kt_kernel_ext, m) {
 }
 
 static void warmup_cpptrace() {
+#if KTRANSFORMERS_HAS_CPPTRACE
   // 避免第一次调用触发 lazy-loading（malloc 等） :contentReference[oaicite:7]{index=7}
   cpptrace::frame_ptr buffer[10];
   (void)cpptrace::safe_generate_raw_trace(buffer, 10);
   cpptrace::safe_object_frame frame{};
   cpptrace::get_safe_object_frame(buffer[0], &frame);
+#endif
 }
 
 static void crash_handler(int signo, siginfo_t* /*info*/, void* /*ucontext*/) {
   const char* head = "=== crash: signal received ===\n";
   write(STDERR_FILENO, head, std::strlen(head));
+#if KTRANSFORMERS_HAS_CPPTRACE
   cpptrace::generate_trace().print();
+#endif
   _exit(128 + signo);
 }
 
@@ -1001,4 +1009,3 @@ __attribute__((constructor)) static void install_handlers() {
   sigaction(SIGSEGV, &sa, nullptr);
   sigaction(SIGABRT, &sa, nullptr);
 }
-
